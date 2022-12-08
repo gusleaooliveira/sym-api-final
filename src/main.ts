b021e0d1182d0d1a8a425c6b604bed6c2179157a
gusleaooliveira <gus.leaono@gmail.com>
2022-12-08 02:34:11 -0300

    port

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log'],
+    logger: ["error", "warn", "log"],
   });
 
   app.enableCors();
 
   const config = new DocumentBuilder()
-    .setTitle('Schedule Your Month')
-    .setDescription('Cadastre seus gastos')
-    .setVersion('0.0.1')
+    .setTitle("Schedule Your Month")
+    .setDescription("Cadastre seus gastos")
+    .setVersion("0.0.1")
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
-  await app.listen(6007);
+  await app.listen(6001);
 }
 bootstrap();
+ 
